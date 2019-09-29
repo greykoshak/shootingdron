9ebff3396f9b7db9a76b77f25e3ac74a3fbedb89
greykoshak <greykoshak@gmail.com>
2019-09-30 02:18:01 +0300

    Принадлежность точки области

--- flying10.py
+++ flying10.py
@@ -26,7 +26,7 @@ class Drone:
 
     def set_full_charge(self, new_value):
         self.full_charge -= new_value
-        print("Остаток энергии: {:9.2f}".format(self.get_full_charge()))
+        print("Остаток энергии: {:5.2f}".format(self.get_full_charge()))
         return
 
 
@@ -44,7 +44,6 @@ class CanvasDrone:
 class PositionDrone:
     def __init__(self, obj_drone):
         self.my_drone = obj_drone
-        self.radius = self.my_drone.max_radius()
         self.points = list()  # Координаты движения дрона
         root.bind('<Button-1>', self.drawing_circle)
 
@@ -55,38 +54,61 @@ class PositionDrone:
 
     def drawing_circle(self, event):
         self.points.append((int(event.x), int(event.y)))
-        can.create_oval(self.points[0][0] - self.radius, self.points[0][1] + self.radius,
-                        self.points[0][0] + self.radius, self.points[0][1] - self.radius, fill="yellow")
+        radius = self.my_drone.max_radius()
+
+        can.create_oval(self.points[0][0] - radius, self.points[0][1] + radius,
+                        self.points[0][0] + radius, self.points[0][1] - radius, fill="yellow", width=2)
         self.drawing_point()
         root.bind('<Button-1>', self.drawing_ellipse)
 
     def drawing_ellipse(self, event):
-        tempX = int(event.x)
-        tempY = int(event.y)
-        self.points.append((tempX, tempY))
+        temp_x = int(event.x)
+        temp_y = int(event.y)
 
-        if self.is_belong_to_circle(tempX, tempY):
+        if self.is_belong_to_area(temp_x, temp_y):
+            self.points.append((temp_x, temp_y))
             self.drawing_point()
 
             # Считаем энергию, которую затратил дрон для полета в эту точку и сделал фото
             ind = len(self.points) - 1
-            dist = (self.points[ind][0], self.points[ind][1], self.points[ind - 1][0], self.points[ind - 1][1])
-            delta = self.my_drone.distance(dist)
+            coord = (self.points[ind][0], self.points[ind][1], self.points[ind - 1][0], self.points[ind - 1][1])
+            delta = self.my_drone.distance(coord)
             delta_energy = delta * self.my_drone.delta_e_flying + self.my_drone.delta_e_shooting
             print("dist {:9.2f} energy {:9.2f}".format(delta, delta_energy))
 
             self.my_drone.set_full_charge(delta_energy)
             e1 = self.my_drone.get_full_charge()
-            focus_a = ((e1 - self.my_drone.delta_e_shooting) / self.my_drone.delta_e_flying - delta) / 2
+            focus_a = (e1 - self.my_drone.delta_e_shooting) / (2 * self.my_drone.delta_e_flying) - delta / 2
+            x = (e1 - self.my_drone.delta_e_shooting) / (2 * self.my_drone.delta_e_flying)
+            focus_b = (x * x - delta * delta / 4) ** 0.5
+            print("a: {:5.2f} b: {:5.2f} e1: {:5.2f}".format(2 * focus_a + delta, 2 * x, e1))
 
-            can.create_oval(self.points[0][0] - focus_a, self.points[0][1] + focus_a,
-                            self.points[ind][0] + focus_a, self.points[ind][1] - focus_a)
+            can.create_oval(self.points[0][0] - focus_b, self.points[0][1] + focus_a,
+                            self.points[ind][0] + focus_b, self.points[ind][1] - focus_a, outline="blue", width=2)
         else:
             print("Not belong")
 
-    def is_belong_to_circle(self, tempX, tempY):
-        return ((tempX - self.points[0][0]) ** 2 + (tempY - self.points[0][1]) ** 2) \
-               <= self.radius ** 2
+    def is_belong_to_area(self, temp_x, temp_y):
+        if len(self.points) == 1:
+            radius = self.my_drone.max_radius()
+            return ((temp_x - self.points[0][0]) ** 2 + (temp_y - self.points[0][1]) ** 2) <= radius ** 2
+        else:
+            ind = len(self.points) - 1
+
+            # Расстояние до фокуса а
+            coord = (self.points[ind][0], self.points[ind][1], temp_x, temp_x)
+            focus_a = self.my_drone.distance(coord)
+
+            # Расстояние до фокуса b
+            coord = (self.points[0][0], self.points[0][1], temp_x, temp_x)
+            focus_b = self.my_drone.distance(coord)
+
+            # Вычисленная точка на эллипсе
+            e1 = self.my_drone.get_full_charge()
+            border = (e1 - self.my_drone.delta_e_shooting) / (2 * self.my_drone.delta_e_flying)
+            print("a: {:5.2f} b: {:5.2f} border: {:5.2f}".format(focus_a, focus_b, 2 * border))
+
+            return focus_a + focus_b + self.my_drone.delta_e_shooting <= 2 * border
 
 
 root = Tk()
